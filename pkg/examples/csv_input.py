afd7c8b7ff5a6c998bc45621ee2e2e2e5a771373
sebastian-ahmed <76592277+sebastian-ahmed@users.noreply.github.com>
2022-03-13 08:27:08 -0700

    Add examples with VSCode launch file

---
+++ pkg/examples/csv_input.py
@@ -0,0 +1,12 @@
+from pkg.core.StockTransactor import StockTransactor
+
+def main():
+    file_name = './pkg/examples/stocks_example.csv'
+    stock_data = StockTransactor(file_name)
+
+    stock_data.print_sales_report()
+    stock_data.print_holdings_report()
+    stock_data.write_sales_file()
+    
+if __name__ == '__main__':
+    main()
